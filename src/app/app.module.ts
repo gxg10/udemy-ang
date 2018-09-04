a9bf47afe9a7bbd8ee98cc38da11ef100b8754cf
George Brande <george.brande@gmail.com>
2018-09-04 16:36:30 +0300

    video nr 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { ShopingListService } from './shopping-list/shoping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { RecipesModule } from './recipes/recipes.module';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    DropdownDirective,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,AppRoutingModule,RecipesModule
-    
+    HttpModule, AppRoutingModule, RecipesModule,
+    SharedModule,
+    ShoppingListModule
+
     ],
   providers: [ShopingListService,
   RecipeService, DataStorageService],
