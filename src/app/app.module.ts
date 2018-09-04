57ae43ebd689796e578d0388c324e739462b037b
Brande George <george.brande@gmail.com>
2018-09-04 19:15:58 +0300

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,22 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ShopingListService } from './shopping-list/shoping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule, AppRoutingModule, RecipesModule,
+    HttpModule, AppRoutingModule,
     SharedModule,
-    ShoppingListModule
-
+    ShoppingListModule,
+    CoreModule
     ],
-  providers: [ShopingListService,
-  RecipeService, DataStorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { RecipesComponent } from "./recipes.component";
-import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
-import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 const recipesRoutes: Routes = [
-    { path: 'recipes',
+    { path: '',
     component: RecipesComponent, children: [
         {path: '',
         component: RecipeStartComponent},
